5311dfaa166a84868606e8254ed4ef19532ed10b
aplosaletheia <aplosaletheia@gmail.com>
2026-09-21 13:30:03 +0530

    really sorry, I forgot to upload this to github, I had a working demo in which this is visible hope you can allow this.

---
+++ src/ft.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include "api.h"
+
+
+
+
+freqDomainST_s dft(audioInfo_s audioInfo, float phase)
+{
+    freqDomainST_s result = {(size_t)audioInfo.sampleRate/2*audioInfo.sampleDuration};
+    result.amps = malloc(result.freqCount*sizeof(*result.amps));
+    size_t i = 0;
+    for (float f = 1/audioInfo.sampleDuration; f <= audioInfo.sampleRate/2 ; f += 1/audioInfo.sampleDuration, i++)
+    {
+        float x = 0;
+        float y = 0;
+
+        float delta = (2*M_PI*f)/audioInfo.sampleRate; //increase per sample
+        float deltaSin = sinf(delta);
+        float deltaCos = cosf(delta);
+        float currSin = sinf(phase*2*M_PI*f);
+        float currCos = cosf(phase*2*M_PI*f);
+        float sinTemp;
+        for (size_t j = 0; j < audioInfo.sampleCount; j++)
+        {
+            x += audioInfo.samples[j]*currSin;
+            y += audioInfo.samples[j]*currCos;
+            sinTemp = currSin;
+            currSin = currSin*deltaCos + currCos*deltaSin;
+            currCos = currCos*deltaCos - sinTemp*deltaSin;
+        }
+        result.amps[i] = sqrtf((x*x + y*y)) * (2.0f / audioInfo.sampleCount);
+        //printf("%f - %f\n", f, result.amps[i]);
+    }
+    return result;
+}
